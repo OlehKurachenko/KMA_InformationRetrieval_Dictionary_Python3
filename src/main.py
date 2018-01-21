005629e4a78858a932756fac52d8438857fb2817
OlehKurachenko <oleh.kurachenko@gmail.com>
2018-01-21 23:43:56 +0200

    main finished

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
-from src import textFileSimplestTokenizer, simplestArrayDictionary
-
+from src import textFileSimplestTokenizer, simplestArrayDictionary, simplestSetDictionary
+import glob
 
 def main():
     array_dictionary = simplestArrayDictionary.SimplestArrayDictionary()
+    set_dictionary = simplestSetDictionary.SimplestSetDictionary()
     try:
-        li = textFileSimplestTokenizer.TestFileSimplestTokenizer("Data/lorem_ipsum.txt")
-        for word in li.token_generator:
-            array_dictionary.add_word(word)
-        print(array_dictionary.__str__())
-        array_dictionary.export_dictionary("data/dic1.txt")
+        for filename in glob.glob('Data/Egmont.txt'):
+            fst = textFileSimplestTokenizer.TestFileSimplestTokenizer(filename)
+            for word in fst.token_generator:
+                array_dictionary.add_word(word)
+                set_dictionary.add_word(word)
     except FileNotFoundError:
         print("File not opened")
+    array_dictionary.export_dictionary("data/arr_dic.txt")
+    set_dictionary.export_dictionary("data/set_dic.txt")
 
     try:
-        array_dictionary2 = simplestArrayDictionary.SimplestArrayDictionary("data/dic1.txt")
-        print(array_dictionary2)
+        array_dictionary2 = simplestArrayDictionary.SimplestArrayDictionary("data/set_dic.txt")
+        set_dictionary2 = simplestSetDictionary.SimplestSetDictionary("data/arr_dic.txt")
+        array_dictionary2.export_dictionary("data/arr_dic2.txt")
+        set_dictionary2.export_dictionary("data/set_dic2.txt")
     except FileNotFoundError:
-        print("Dictionary file not found")
+        print("Dictionary file not opened")
 
 
 if __name__ == "__main__":
