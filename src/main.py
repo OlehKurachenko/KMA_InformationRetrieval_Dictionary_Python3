d5d1c31f56c7cbc7008c448cab2557bc2613b86b
OlehKurachenko <oleh.kurachenko@gmail.com>
2018-01-22 10:03:57 +0200

    packeges names changed

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from src import textFileSimplestTokenizer, simplestArrayDictionary, simplestSetDictionary
+from src import text_file_simplest_tokenizer, simplest_array_dictionary, simplest_set_dictionary
 import glob
 
 def main():
-    array_dictionary = simplestArrayDictionary.SimplestArrayDictionary()
-    set_dictionary = simplestSetDictionary.SimplestSetDictionary()
+    array_dictionary = simplest_array_dictionary.SimplestArrayDictionary()
+    set_dictionary = simplest_set_dictionary.SimplestSetDictionary()
     try:
         for filename in glob.glob('Data/Egmont.txt'):
-            fst = textFileSimplestTokenizer.TestFileSimplestTokenizer(filename)
+            fst = text_file_simplest_tokenizer.TestFileSimplestTokenizer(filename)
             for word in fst.token_generator:
                 array_dictionary.add_word(word)
                 set_dictionary.add_word(word)
@@ -16,8 +16,8 @@ def main():
     set_dictionary.export_dictionary("data/set_dic.txt")
 
     try:
-        array_dictionary2 = simplestArrayDictionary.SimplestArrayDictionary("data/set_dic.txt")
-        set_dictionary2 = simplestSetDictionary.SimplestSetDictionary("data/arr_dic.txt")
+        array_dictionary2 = simplest_array_dictionary.SimplestArrayDictionary("data/set_dic.txt")
+        set_dictionary2 = simplest_set_dictionary.SimplestSetDictionary("data/arr_dic.txt")
         array_dictionary2.export_dictionary("data/arr_dic2.txt")
         set_dictionary2.export_dictionary("data/set_dic2.txt")
     except FileNotFoundError:
