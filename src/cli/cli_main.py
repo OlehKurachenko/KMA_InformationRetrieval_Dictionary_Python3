b9c6ae65a0b664ac788de39bf5e0ccb60df5aa4e
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-01-26 14:57:58 +0200

    now CLA handle commands

--- src/cli/cli_main.py
+++ src/cli/cli_main.py
@@ -33,7 +33,6 @@ class CLIMain:
 
     --help  prints help
 
-        TODO finish doc
     """
 
     __target = []
@@ -52,8 +51,17 @@ class CLIMain:
 
     @staticmethod
     def __handle_command(argv):
-        print(argv)
-        # TODO write
+        for cla in argv[1:]:
+            if cla[:2] == "--":
+                if len(argv) == 2:
+                    if argv[1] == "--help":
+                        print(CLIMain.__HELP_MESSAGE)
+                    else:
+                        print(argv[0] + ": wrong command entered, try --help")
+                else:
+                    print(argv[0] + ": usage of command with any other command line "
+                                    "argument is forbidden")
+                return False
         return True
 
     @staticmethod
@@ -70,7 +78,8 @@ class CLIMain:
                                                             "[-o out_filename] "
                                                             "[arguments]")
             print(sys.argv[0] + ": use " + sys.argv[0] + " --help")
-        # TODO check "--" - type flags
+        if not CLIMain.__handle_command(sys.argv):
+            return
         i = 1
         while i < len(sys.argv):
             if sys.argv[i][0] == '-':
@@ -78,3 +87,22 @@ class CLIMain:
             else:
                 CLIMain.__target.append(sys.argv[i])
             i += 1
+
+    __HELP_MESSAGE = "Flag is a command line argument which starts with '-' There " \
+                     "are two types of flags: one, that require arguments, and one " \
+                     "which not (called non-arg flags). Non-arg flags can be grouped " \
+                     "in one string (like '-nmlf', for ex.). Therefore, it is forbidden" \
+                     " to have arg flag grouped with any other flag.\n\n" \
+                     "Arguments without flag are taken as target files.\n\n" \
+                     "-i [dic_filename] : specifies an input dictionary, which will be " \
+                     "imported before target files analysis, and new tokens will be added" \
+                     " to this dictionary.\n\n" \
+                     "-I [dic_filename] : the same behaviour as -i, but output filename is" \
+                     " specified the same as [dic_filename].\n\n" \
+                     "-o  [out_filename] : specifies filename for output file. If file do not" \
+                     " exist it will be created, otherwise existing file will be rewritten.\n\n" \
+                     "-a  forces program to use array (list) as data structure\n\n" \
+                     "-t  forces program to print traces\n\n" \
+                     "Commands are used to get some data about program. Commands line argument" \
+                     " ofcommand starts with \"--\", and can be only one CLA for call.\n\n" \
+                     "--help  prints help\n"
