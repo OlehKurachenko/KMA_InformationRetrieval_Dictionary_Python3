4896acbb18a39779fb8b70c9459705821d8153cc
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-01-27 23:09:02 +0200

    assertions for wrong type arguments checked for text_file_tokenizer

--- src/text_file_tokenizers/text_file_simplest_tokenizer.py
+++ src/text_file_tokenizers/text_file_simplest_tokenizer.py
@@ -9,6 +9,8 @@ class TestFileSimplestTokenizer:
 
     @staticmethod
     def __lazy_line_generator(filename):
+        assert type(filename) == str, TestFileSimplestTokenizer.WRONG_ARGUMENT_TYPE_MESSAGE
+
         with open(filename) as file:
             for line in file:
                 strsplit = split("[ .,!?:;\"'\t\r\n\v\f]+", line)
@@ -17,8 +19,12 @@ class TestFileSimplestTokenizer:
                         yield token.lower()
 
     def __init__(self, filename):
+        assert type(filename) == str, TestFileSimplestTokenizer.WRONG_ARGUMENT_TYPE_MESSAGE
+
         self._token_generator = TestFileSimplestTokenizer.__lazy_line_generator(filename)
 
     @property
     def token_generator(self):
         return self._token_generator
+
+    WRONG_ARGUMENT_TYPE_MESSAGE = "Wrong argument type"
