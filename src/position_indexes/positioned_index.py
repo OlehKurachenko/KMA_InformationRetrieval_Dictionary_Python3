2f68c815989af5b435f605baa9de5c4e724b339f
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-02-04 23:32:27 +0200

    simplest position search over position_inverted_index written

--- src/position_indexes/positioned_index.py
+++ src/position_indexes/positioned_index.py
@@ -13,6 +13,8 @@
 from src.indexes.abs_binary_searcher import ABSBinarySearcher
 from src.text_file_tokenizers.text_file_simplest_tokenizer import TestFileSimplestTokenizer
 
+from re import split
+
 
 class PositionInvertedIndex(ABSBinarySearcher):
     # TODO write documentation
@@ -29,7 +31,7 @@ class PositionInvertedIndex(ABSBinarySearcher):
                     ii_list = self.__inverted_index[word]
                     if not (ii_list and ii_list[-1][0] == i):
                         ii_list.append((i, []))
-                    ii_list[1].append(j)
+                    ii_list[-1][1].append(j)
         else:
             pass  # TODO load existing incidence matrix
 
@@ -56,63 +58,100 @@ class PositionInvertedIndex(ABSBinarySearcher):
     def perform_request(self, request):
         assert type(request) == str, PositionInvertedIndex.__WRONG_ARGUMENT_TYPE
 
-        request += ' '
-        words_list = []
-        operations_list = []
-        i = 0
-        is_value = True
-        while i < len(request):
-            while i < len(request) and request[i] == ' ' :
-                i += 1
-            if i == len(request):
-                break
-            if request[i] == '(':
-                operations_list.append('(')
-                i += 1
-            elif request[i] == ')':
-                temp_opp = operations_list.pop()
-                while temp_opp != '(':
-                    if temp_opp == 'or':
-                        words_list.append(
-                            PositionInvertedIndex.lists_merge(words_list.pop(), words_list.pop()))
-                    elif temp_opp == 'and':
-                        words_list.append(
-                            PositionInvertedIndex.lists_intersect(words_list.pop(), words_list.pop()))
-                    else:
-                        raise Exception
-                    temp_opp = operations_list.pop()
-                i += 1
-            else:
-                j = i
-                while request[j] != ' ':
-                    j += 1
-                word = request[i:j]
-                i = j
-                if is_value:
-                    if word in self.__inverted_index:
-                        words_list.append(self.__inverted_index[word])
-                    else:
-                        words_list.append([])
-                else:
-                    operations_list.append(word)
-                is_value = not is_value
-        while operations_list:
-            temp_opp = operations_list.pop()
-            if temp_opp == 'or':
-                words_list.append(
-                    PositionInvertedIndex.lists_merge(words_list.pop(), words_list.pop()))
-            elif temp_opp == 'and':
-                words_list.append(
-                    PositionInvertedIndex.lists_intersect(words_list.pop(), words_list.pop()))
-            else:
-                print(temp_opp)
-                raise Exception
-        if len(words_list) != 1 or operations_list:
-            raise Exception
+        strsplit = split("[ .,!?:;\"'\t\r\n\v\f]+", request)
+        if len(strsplit) != 3:
+            return False
+        # noinspection PyBroadException
+        distance = 0
+        try:
+            distance = int(strsplit[2])
+        except Exception:
+            return False
+        # TODO move to another method
+        i1 = 0
+        i2 = 0
+        l1 = self.__inverted_index[strsplit[0]]
+        l2 = self.__inverted_index[strsplit[1]]
         res = []
-        for i in words_list.pop():
-            res.append(self.__files[i])
+        while (i1 < len(l1)) and (i2 < len(l2)):
+            if l1[i1][0] == l2[i2][0]:
+                j1 = 0
+                j2 = 0
+                while (j1 < len(l1[i1][1])) and (j2 < len(l2[i2][1])):
+                    # TODO fix b b 1 => similar to non-zero
+                    if abs(l1[i1][1][j1] - l2[i2][1][j2]) <= distance:
+                        res.append(l1[i1][0])
+                        break
+                    elif l1[i1][1][j1] < l2[i2][1][j2]:
+                        j1 += 1
+                    else:
+                        j2 += 1
+                i1 += 1
+                i2 += 1
+            elif l1[i1][0] < l2[i2][0]:
+                i1 += 1
+            else:
+                i2 += 1
         return res
+
+        # request += ' '
+        # words_list = []
+        # operations_list = []
+        # i = 0
+        # is_value = True
+        # while i < len(request):
+        #     while i < len(request) and request[i] == ' ' :
+        #         i += 1
+        #     if i == len(request):
+        #         break
+        #     if request[i] == '(':
+        #         operations_list.append('(')
+        #         i += 1
+        #     elif request[i] == ')':
+        #         temp_opp = operations_list.pop()
+        #         while temp_opp != '(':
+        #             if temp_opp == 'or':
+        #                 words_list.append(
+        #                     PositionInvertedIndex.lists_merge(words_list.pop(), words_list.pop()))
+        #             elif temp_opp == 'and':
+        #                 words_list.append(
+        #                     PositionInvertedIndex.lists_intersect(words_list.pop(),
+        # words_list.pop()))
+        #             else:
+        #                 raise Exception
+        #             temp_opp = operations_list.pop()
+        #         i += 1
+        #     else:
+        #         j = i
+        #         while request[j] != ' ':
+        #             j += 1
+        #         word = request[i:j]
+        #         i = j
+        #         if is_value:
+        #             if word in self.__inverted_index:
+        #                 words_list.append(self.__inverted_index[word])
+        #             else:
+        #                 words_list.append([])
+        #         else:
+        #             operations_list.append(word)
+        #         is_value = not is_value
+        # while operations_list:
+        #     temp_opp = operations_list.pop()
+        #     if temp_opp == 'or':
+        #         words_list.append(
+        #             PositionInvertedIndex.lists_merge(words_list.pop(), words_list.pop()))
+        #     elif temp_opp == 'and':
+        #         words_list.append(
+        #             PositionInvertedIndex.lists_intersect(words_list.pop(), words_list.pop()))
+        #     else:
+        #         print(temp_opp)
+        #         raise Exception
+        # if len(words_list) != 1 or operations_list:
+        #     raise Exception
+        # res = []
+        # for i in words_list.pop():
+        #     res.append(self.__files[i])
+        # return res
 
     # TODO create outher package for utill fucnitons
     @staticmethod
@@ -163,4 +202,3 @@ class PositionInvertedIndex(ABSBinarySearcher):
         return lr
 
     __WRONG_ARGUMENT_TYPE = "Wrong argument type"
-
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,7 @@
 from src.indexes.incidence_matrix import IncidenceMatrix
 from src.indexes.inverted_index import InvertedIndex
 from src.two_words_index.two_words_inverted_index import TwoWordsInvertedIndex
+from src.position_indexes.positioned_index import PositionInvertedIndex
 import glob
 
 def practice2():
@@ -17,9 +18,11 @@ def practice3():
     a = list(glob.glob("DicData/Samples/*"))
     twii = TwoWordsInvertedIndex(a)
     twii.export("data/twii.txt")
+    pii = PositionInvertedIndex(a)
+    pii.export("data/pii.txt")
 
     print(twii.perform_request("d d"))
-
+    print(pii.perform_request("b d 4"))
 def main():
     practice3()
 
