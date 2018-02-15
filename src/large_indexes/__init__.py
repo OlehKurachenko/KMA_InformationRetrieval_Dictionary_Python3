a741860bfb3c0635b2f9b1f968dfd8fb85f2a7d8
OlehKurachenko <oleh.kurachenko@gmail.com>
2018-02-15 23:06:41 +0200

    large indexes package crated

---
+++ src/large_indexes/__init__.py
@@ -0,0 +1,11 @@
+#
+#   Created by Oleh Kurachenko
+#          aka soll_nevermind aka okurache
+#   on 2018-02-15T23:05:23Z as a part of KMA_InformationRetrieval_Dictionaty_Python3
+#
+#   ask     oleh.kurachenko@gmail.com , I'm ready to help
+#   GitHub  https://github.com/OlehKurachenko
+#   rate&CV http://www.linkedin.com/in/oleh-kurachenko-6b025b111
+#  
+
+__all__ = []
