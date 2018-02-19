406ce0a367fe6861844e30169f8d1b3da17c6033
OlehKurachenko <oleh.kurachenko@gmail.com>
2018-02-19 23:47:15 +0200

    spimi test version written

--- src/large_indexes/spimi_index.py
+++ src/large_indexes/spimi_index.py
@@ -9,12 +9,18 @@
 #  
 
 import io
+import sys
+import time
+import os
+from functools import reduce
+from src.text_file_tokenizers.text_file_simplest_tokenizer import TextFileSimplestTokenizer
 
 
 # noinspection PyUnresolvedReferences
 class SPIMIndex:
     # TODO write docs
     def __init__(self, documents: "list of str", log_ostream: io.IOBase = False):
+        # TODO write logs
         assert type(documents) == list, SPIMIndex.WRONG_ARGUMENT_TYPE_MESSAGE
         assert len(list(filter(lambda x: type(x) != str,
                                documents))) == 0, SPIMIndex.WRONG_ARGUMENT_TYPE_MESSAGE
@@ -23,12 +29,68 @@ class SPIMIndex:
 
         self.__log_ostream = log_ostream
         self.__documents = documents
-        self.__log_ostream.write()
+        # TODO: log self.__log_ostream.write()
+        self.__id = int(time.time())
+        self.__dirname = "data/spimi/" + str(self.__id)
+        self.__file_id = 1
+        # TODO move constant to static
+        os.makedirs(self.__dirname)
+        self.__blocks = []
         # TODO add dynamic block size
-        # TODO finish method
+        self.__block_size = SPIMIndex.DEFAULT_BLOCK_SIZE
+
+        block_size = 0
+        block = []
+
+        for term_pair in self.__term_doc_pairs_generator():
+            block.append(term_pair)
+            block_size += SPIMIndex.DEFAULT_EMPTY_STR_SIZE + SPIMIndex.DEFAULT_INT_SIZE + len(
+                term_pair)
+            if block_size > self.__block_size:
+                block.sort()
+                self.__export_block(block)
+                block = []
+                block_size = 0
+
+        self.index_name = reduce(lambda file1, file2: self.__merge_blockfiles(file1, file2),
+                                   self.__blocks)
 
     def __term_doc_pairs_generator(self):
-        # TODO write
+        for document_id, document in enumerate(self.__documents):
+            for word in TextFileSimplestTokenizer(document).token_generator:
+                yield (word, document_id)
+
+    def __export_block(self, block: "list of tuples"):
+        self.__blocks.append(self.__dirname + "/block_" + str(self.__file_id) + ".txt")
+        self.__file_id += 1
+        with open(self.__blocks[-1], "w+") as export_file:
+            for tup in block:
+                export_file.write(tup[0] + " " + str(tup[1]) + "\n")
+
+    def __merge_blockfiles(self, file1: str, file2: str):
+        new_blockfile_name = self.__dirname + "/block_" + str(self.__file_id) + "m.txt"
+        self.__file_id += 1
+        with open(file1, "r") as file_1:
+            with open(file2, "r") as file_2:
+                with open(new_blockfile_name, "w+") as new_blockfile:
+                    file1_line = file_1.readline()
+                    file2_line = file_2.readline()
+                    while file1_line and file2_line:
+                        if file1_line < file2_line:
+                            new_blockfile.write(file1_line)
+                            file1_line = file_1.readline()
+                        else:
+                            new_blockfile.write(file2_line)
+                            file2_line = file_2.readline()
+                    while file1_line:
+                        new_blockfile.write(file1_line)
+                        file1_line = file_1.readline()
+                    while file2_line:
+                        new_blockfile.write(file2_line)
+                        file2_line = file_2.readline()
+                    return new_blockfile_name
 
     WRONG_ARGUMENT_TYPE_MESSAGE = "Wrong argument type"
     DEFAULT_BLOCK_SIZE = 1024 * 1024
+    DEFAULT_EMPTY_STR_SIZE = sys.getsizeof("")
+    DEFAULT_INT_SIZE = sys.getsizeof(DEFAULT_BLOCK_SIZE)
--- src/test_main.py
+++ src/test_main.py
@@ -2,6 +2,7 @@ from src.indexes.incidence_matrix import IncidenceMatrix
 from src.indexes.inverted_index import InvertedIndex
 from src.two_words_index.two_words_inverted_index import TwoWordsInvertedIndex
 from src.position_indexes.positioned_index import PositionInvertedIndex
+from src.large_indexes.spimi_index import SPIMIndex
 import glob
 
 def practice2():
@@ -23,9 +24,11 @@ def practice3():
 
     print(twii.perform_request("d d"))
     print(pii.perform_request("b d 4"))
-def main():
-    practice3()
 
+def main():
+    files = list(glob.glob("DicData/Books/*"))
+    spimi = SPIMIndex(files)
+    print(spimi.index_name)
 
 if __name__ == "__main__":
     main()
